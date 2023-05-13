ed615858206035d56916bd0d4cf47fbae278044f
amnasyed1 <123895397+amnasyed1@users.noreply.github.com>
2023-05-12 23:24:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,15 @@ df = pd.read_csv('./data/rxdetail2018new.csv')
 
 app = FastAPI()
 
-
 @app.get('/')
 def home():
     return {'this is an API service for MN code details'}
-
 
 @app.get('/preview')
 async def preview():
     top10rows = df.head(10)
     result = top10rows.to_json(orient="records")
     return result
-
 
 @app.get('/launch_year/{value}')
 async def icdcode(value):
@@ -32,11 +29,10 @@ async def icdcode(value):
     else:
         return filtered.to_json(orient="records")
 
-
-@app.get('/launch_year/{value}/encounter_count/{value2}')
+@app.get('/dosage_form/{value}/encounter_count/{value2}')
 async def icdcode2(value, value2):
     print('value: ', value)
-    filtered = df[df['launch_year'] == value]
+    filtered = df[df['dosage_form'] == value]
     filtered2 = filtered[filtered['encounter_count'] == value2]
     if len(filtered2) <= 0:
         return 'There is nothing here'
